d9b95aecc77badc4321c6503011de45b60fb8a1f
harsh <harsh.cmptechno@gmail.com>
2026-06-09 19:03:40 +0530

    added code for error in login

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -17,11 +17,6 @@ const logger = winston.createLogger({
     new winston.transports.Console({
       format: combine(colorize(), timestamp({ format: 'HH:mm:ss' }), logFormat),
     }),
-    new winston.transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-    }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
